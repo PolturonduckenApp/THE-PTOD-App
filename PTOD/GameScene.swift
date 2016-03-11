005bbfe69cd1f3ecb2e118dea70b407d55a91da0
Andrew Turley <aturley123@gmail.com>
2016-03-11 14:30:35 -0500

    Working orangutan that moves to x location of tap and rotates

--- PTOD/GameScene.swift
+++ PTOD/GameScene.swift
@@ -9,6 +9,7 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    let sprite = SKSpriteNode(imageNamed:"Orangutan")
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
@@ -16,6 +17,13 @@ class GameScene: SKScene {
         myLabel.fontSize = 45
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
+        sprite.xScale = 0.5
+        sprite.yScale = 0.5
+        
+        sprite.position.x = 100
+        sprite.position.y = 100
+        
+        self.addChild(sprite)
         self.addChild(myLabel)
     }
     
@@ -25,17 +33,11 @@ class GameScene: SKScene {
         for touch in touches {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Orangutan")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            sprite.position.x = location.x
             
             let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
             
             sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
     }
    
